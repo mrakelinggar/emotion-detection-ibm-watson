84fcefe76859d9933090ca850db8620b79918b46
Rake Anggoro <rake.linggar@gmail.com>
2026-01-03 20:49:47 -0500

    Edited server.py for cleaner code

--- server.py
+++ server.py
@@ -9,13 +9,14 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
-    ''' This code receives the text from the HTML interface and 
+    ''' This code receives the text from the HTML interface and
         runs emotion detection over it using emotion_detector()
         function. The output formatted text
     '''
+
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
-    
+
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
 
@@ -23,13 +24,13 @@ def sent_analyzer():
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
-    formatted_text = ( 
-        f"For the given statement, the system response is " 
-        f"'anger': {response['anger']}, " 
-        f"'disgust': {response['disgust']}, " 
-        f"'fear': {response['fear']}, " 
-        f"'joy': {response['joy']} and " 
-        f"'sadness': {response['sadness']}. " 
+    formatted_text = (
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, "
+        f"'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. "
         f"The dominant emotion is {response['dominant_emotion']}." )
 
     # Return a formatted text
@@ -43,6 +44,5 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
+    # This functions executes the flask app and deploys it on localhost:5000
     app.run('0.0.0.0', port=5000)
